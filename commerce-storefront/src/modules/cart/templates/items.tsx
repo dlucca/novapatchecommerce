37e5c88637e685775e21e869db95c0f0add179e3
EstefaniaDel <estefaniadelvalle0987@gmail.com>
2025-10-13 00:02:19 -0300

    feat: update ItemsTemplate component to improve layout and localization for better user experience

--- commerce-storefront/src/modules/cart/templates/items.tsx
+++ commerce-storefront/src/modules/cart/templates/items.tsx
@@ -1,6 +1,6 @@
 import repeat from "@lib/util/repeat"
 import { HttpTypes } from "@medusajs/types"
-import { Heading, Table } from "@medusajs/ui"
+import { Table } from "@medusajs/ui"
 
 import Item from "@modules/cart/components/item"
 import SkeletonLineItem from "@modules/skeletons/components/skeleton-line-item"
@@ -13,17 +13,17 @@ const ItemsTemplate = ({ cart }: ItemsTemplateProps) => {
   const items = cart?.items
   return (
     <div>
-      <div className="pb-3 flex items-center">
-        <Heading className="text-[2rem] leading-[2.75rem]">Cart</Heading>
+      <div className="pb-4 flex items-center">
+        <h2 className="text-2xl font-bold text-gray-900">Productos</h2>
       </div>
       <Table>
         <Table.Header className="border-t-0">
-          <Table.Row className="text-ui-fg-subtle txt-medium-plus">
-            <Table.HeaderCell className="!pl-0">Item</Table.HeaderCell>
+          <Table.Row className="text-gray-600 text-sm font-medium">
+            <Table.HeaderCell className="!pl-0">Producto</Table.HeaderCell>
             <Table.HeaderCell></Table.HeaderCell>
-            <Table.HeaderCell>Quantity</Table.HeaderCell>
+            <Table.HeaderCell>Cantidad</Table.HeaderCell>
             <Table.HeaderCell className="hidden small:table-cell">
-              Price
+              Precio
             </Table.HeaderCell>
             <Table.HeaderCell className="!pr-0 text-right">
               Total
