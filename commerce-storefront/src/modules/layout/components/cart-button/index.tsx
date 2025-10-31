1b6611cf60d3011bb5dfbe24ecd8b2330d881414
Estefania Del Valle <estefaniadelvalle0987@gmail.com>
2025-10-30 22:46:02 -0300

    refactor(layout): remove unused scrolled prop from CartButton components

--- commerce-storefront/src/modules/layout/components/cart-button/index.tsx
+++ commerce-storefront/src/modules/layout/components/cart-button/index.tsx
@@ -1,12 +1,8 @@
 import { retrieveCart } from "@lib/data/cart"
 import CartButtonClient from "./cart-button-client"
 
-interface CartButtonProps {
-  scrolled?: boolean
-}
-
-export default async function CartButton({ scrolled = false }: CartButtonProps) {
+export default async function CartButton() {
   const cart = await retrieveCart().catch(() => null)
 
-  return <CartButtonClient cart={cart} scrolled={scrolled} />
+  return <CartButtonClient cart={cart} />
 }
