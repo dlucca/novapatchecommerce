c52fe4005318e9b69f1bc17fe31b8b69efe3ef7e
EstefaniaDel <estefaniadelvalle0987@gmail.com>
2025-11-13 22:00:09 +0000

    fix: update hostname for production environment in next.config.js

--- commerce-storefront/next.config.js
+++ commerce-storefront/next.config.js
@@ -41,7 +41,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "novapatchecommerce-production.up.railway.app",
+        hostname: "novapatchecommerce-production-f896.up.railway.app",
       },
       {
         protocol: "https",
